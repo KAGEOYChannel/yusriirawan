a35da08080c1a9def4bc53df5aea268235718b0f
KAGEOYChannel <73371239+KAGEOYChannel@users.noreply.github.com>
2023-08-12 23:23:13 +0700

    Update jsmang.js

--- jsmang.js
+++ jsmang.js
@@ -1,10 +1,8 @@
 const BUTTON = document.querySelector("button");
-const SYNC = document.querySelector("#sync")
 
 const TOGGLE = () => {
   const IS_PRESSED = BUTTON.matches("[aria-pressed=true]");
-  if (SYNC.checked)
-    document.body.setAttribute("data-dark-mode", IS_PRESSED ? false : true);
+  document.body.setAttribute("data-dark-mode", IS_PRESSED ? false : true);
   BUTTON.setAttribute("aria-pressed", IS_PRESSED ? false : true);
 };
 
