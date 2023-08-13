b2caaee14a4b043987dd46a4165d301c88944a1b
KAGEOYChannel <73371239+KAGEOYChannel@users.noreply.github.com>
2023-08-13 07:18:55 +0700

    Update load.js

--- load.js
+++ load.js
@@ -1,9 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const loadingOverlay = document.querySelector('.loading-overlay');
-  const content = document.querySelector('.content');
-
-  setTimeout(function () {
-    loadingOverlay.style.display = 'none'; // Sembunyikan overlay setelah loading selesai
-    content.style.display = 'block'; // Tampilkan konten halaman utama
-  }, 4000); // Waktu simulasi loading (dalam milidetik)
+$(document).ready(function() {
+ 
+  // Fakes the loading setting a timeout
+    setTimeout(function() {
+        $('body').addClass('loaded');
+    }, 4500);
+ 
 });
