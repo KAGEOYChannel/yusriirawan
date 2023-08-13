597b12e30a2738cc4f34b4164375937a3bfbe631
KAGEOYChannel <73371239+KAGEOYChannel@users.noreply.github.com>
2023-08-13 11:07:27 +0700

    Update load.js

--- load.js
+++ load.js
@@ -1,8 +1,7 @@
-$(document).ready(function() {
- 
-  // Fakes the loading setting a timeout
-    setTimeout(function() {
-        $('body').addClass('loaded');
-    }, 3500);
- 
+document.addEventListener("DOMContentLoaded", function () {
+  // Simulate loading process
+  setTimeout(function () {
+    // Add the "loaded" class to the body to trigger loaded styles
+    document.body.classList.add("loaded");
+  }, 3000); // Adjust the delay as needed
 });
