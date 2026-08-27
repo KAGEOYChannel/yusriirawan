712bce885491833fabeb492f20e7f0885c8e02d0
KAGEOYChannel <73371239+KAGEOYChannel@users.noreply.github.com>
2026-08-27 11:13:44 +0700

    Update vite.config.ts

--- pembelajaran/vite.config.ts
+++ pembelajaran/vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig(() => {
   return {
     base: './',
     plugins: [react(), tailwindcss()],
+    build: {
+      outDir: 'dist',
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
